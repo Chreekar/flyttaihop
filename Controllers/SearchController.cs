e47cd7a68f04991fb127f1ee339dee004c31f97d
Christer Carlsund <christer.carlsund@alstra.se>
2016-09-30 17:00:23 +0200

    loggar om ingen GoogleApiKey har angetts

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -10,8 +10,8 @@ using Flyttaihop.Framework.Models;
 using Flyttaihop.Framework.Parsers;
 using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Flyttaihop.Controllers
@@ -20,12 +20,14 @@ namespace Flyttaihop.Controllers
     public class SearchController : Controller
     {
         private readonly IOptions<ApplicationOptions> _applicationOptions;
+        private readonly ILogger<SearchController> _logger;
         private readonly ICriteriaRepository _criteriaRepository;
         private readonly HemnetParser _hemnetParser;
 
-        public SearchController(IOptions<ApplicationOptions> applicationOptions, ICriteriaRepository criteriaRepository, HemnetParser hemnetParser)
+        public SearchController(IOptions<ApplicationOptions> applicationOptions, ILogger<SearchController> logger, ICriteriaRepository criteriaRepository, HemnetParser hemnetParser)
         {
             _applicationOptions = applicationOptions;
+            _logger = logger;
             _criteriaRepository = criteriaRepository;
             _hemnetParser = hemnetParser;
         }
@@ -35,6 +37,13 @@ namespace Flyttaihop.Controllers
         public async Task<List<SearchResult>> Search()
         {
             var savedCriteria = _criteriaRepository.GetSavedCriteria();
+
+            string googleApiKey = _applicationOptions.Value.GoogleApiKey;
+
+            if (string.IsNullOrWhiteSpace(googleApiKey))
+            {
+                _logger.LogError("GoogleApiKey not specified, skipping duration calculations");
+            }
 
             var result = new List<SearchResult>();
 
@@ -66,67 +75,63 @@ namespace Flyttaihop.Controllers
 
                     item.Durations = new List<Duration>();
 
-                    //Räkna ut avstånd
-                    if (savedCriteria.DurationCriterias.Any())
+                    //Räkna ut tidsåtgång
+                    if (savedCriteria.DurationCriterias.Any() && !string.IsNullOrWhiteSpace(googleApiKey))
                     {
-                        string googleApiKey = _applicationOptions.Value.GoogleApiKey;
-
-                        if (!string.IsNullOrWhiteSpace(googleApiKey))
+                        foreach (var durationCriteria in savedCriteria.DurationCriterias)
                         {
-                            foreach (var durationCriteria in savedCriteria.DurationCriterias)
+                            //TODO: Kör asynkront! Dvs låt flera trådar slå upp mot google samtidigt, och så await i slutet för att vänta in alla innan returnerar
+                            using (var googleClient = new HttpClient())
                             {
-                                using (var googleClient = new HttpClient())
+                                //Dokumentation: https://developers.google.com/maps/documentation/directions/intro
+
+                                googleClient.BaseAddress = new Uri("https://maps.googleapis.com");
+                                string itemAddress = item.Address + "," + item.City;
+                                string requestUri = string.Format("/maps/api/directions/json?origin={0}&destination={1}&key={2}", itemAddress, durationCriteria.Target, googleApiKey);
+                                if (durationCriteria.Type == Duration.TraversalType.Commuting)
                                 {
-                                    //Dokumentation: https://developers.google.com/maps/documentation/directions/intro
-
-                                    googleClient.BaseAddress = new Uri("https://maps.googleapis.com");
-                                    string itemAddress = item.Address + "," + item.City;
-                                    string requestUri = string.Format("/maps/api/directions/json?origin={0}&destination={1}&key={2}", itemAddress, durationCriteria.Target, googleApiKey);
-                                    if (durationCriteria.Type == Duration.TraversalType.Commuting)
-                                    {
-                                        requestUri += "&mode=transit&departure_time=" + "TODO: Unix epoch timestamp för nästa tisdag kl 17";
-                                    }
-                                    else if (durationCriteria.Type == Duration.TraversalType.Walking)
-                                    {
-                                        requestUri += "&mode=walking";
-                                    }
-                                    else if (durationCriteria.Type == Duration.TraversalType.Biking)
-                                    {
-                                        requestUri += "&mode=bicycling";
-                                    }
-
-                                    var googleResult = await googleClient.GetAsync(requestUri);
-                                    var googleResultString = await googleResult.Content.ReadAsStringAsync();
-
-                                    var googleJson = JObject.Parse(googleResultString);
-
-                                    if ((string)googleJson.SelectToken("geocoded_waypoints[0].geocoder_status") != "OK" ||
-                                        (string)googleJson.SelectToken("geocoded_waypoints[1].geocoder_status") != "OK")
-                                    {
-                                        //Inkluderar de objekt vi inte lyckas slå upp för säkerhets skull
-                                        break;
-                                    }
-
-                                    int distanceMeters = (int)googleJson.SelectToken("routes[0].legs[0].distance.value");
-                                    float distanceKilometers = distanceMeters / 1000f;
-
-                                    int durationSeconds = (int)googleJson.SelectToken("routes[0].legs[0].duration.value");
-                                    int durationMinutes = durationSeconds / 60;
-
-                                    if (durationMinutes > durationCriteria.Minutes)
-                                    {
-                                        //Exkluderar de objekt som ligger för långt bort
-                                        excludeItem = true;
-                                        break;
-                                    }
-
-                                    item.Durations.Add(new Duration
-                                    {
-                                        Minutes = durationMinutes,
-                                        Type = durationCriteria.Type,
-                                        Target = durationCriteria.Target
-                                    });
+                                    requestUri += "&mode=transit&departure_time=" + "TODO: Unix epoch timestamp för nästa tisdag kl 17";
                                 }
+                                else if (durationCriteria.Type == Duration.TraversalType.Walking)
+                                {
+                                    requestUri += "&mode=walking";
+                                }
+                                else if (durationCriteria.Type == Duration.TraversalType.Biking)
+                                {
+                                    requestUri += "&mode=bicycling";
+                                }
+
+                                var googleResult = await googleClient.GetAsync(requestUri);
+                                var googleResultString = await googleResult.Content.ReadAsStringAsync();
+
+                                var googleJson = JObject.Parse(googleResultString);
+
+                                if ((string)googleJson.SelectToken("geocoded_waypoints[0].geocoder_status") != "OK" ||
+                                    (string)googleJson.SelectToken("geocoded_waypoints[1].geocoder_status") != "OK")
+                                {
+                                    //Inkluderar de objekt vi inte lyckas slå upp för säkerhets skull
+                                    break;
+                                }
+
+                                int distanceMeters = (int)googleJson.SelectToken("routes[0].legs[0].distance.value");
+                                float distanceKilometers = distanceMeters / 1000f;
+
+                                int durationSeconds = (int)googleJson.SelectToken("routes[0].legs[0].duration.value");
+                                int durationMinutes = durationSeconds / 60;
+
+                                if (durationMinutes > durationCriteria.Minutes)
+                                {
+                                    //Exkluderar de objekt som ligger för långt bort
+                                    excludeItem = true;
+                                    break;
+                                }
+
+                                item.Durations.Add(new Duration
+                                {
+                                    Minutes = durationMinutes,
+                                    Type = durationCriteria.Type,
+                                    Target = durationCriteria.Target
+                                });
                             }
                         }
                     }
